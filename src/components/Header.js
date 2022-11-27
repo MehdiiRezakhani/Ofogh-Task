a4150c4a8bf861ff61f844369d7450c5ccbb4fe0
MehdiRezakhani <mehdiirezakhani@gmail.com>
2022-11-27 15:32:05 +0330

    Header is created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import {  useNavigate } from "react-router-dom";
-import { authAction } from "../redux/authSlice";
-import { uiAction } from "../redux/uiSlice";
+import {useNavigate} from "react-router-dom";
+//redux
+import {useDispatch} from "react-redux";
+import {authAction} from "../redux/authSlice";
+import {uiAction} from "../redux/uiSlice";
+//style
+import '../styles/Header.css';
 
 const Header = ({ title, path, nameButton }) => {
   const dispatch = useDispatch();
@@ -13,29 +16,21 @@ const Header = ({ title, path, nameButton }) => {
     dispatch(
       uiAction.showNotification({
         status: "Success",
-        message: "Logout is succes.",
+        message: "Logout was successful!",
       })
     );
     dispatch(authAction.logoutHandler());
   };
 
   return (
-    <div className="flex justify-between py-3 px-5 bg-slate-300 items-center">
-      <h2 className="capitalize">{title}</h2>
-      <div className="flex space-x-2 justify-center">
+    <header>
+      <h2>Ofogh</h2>
+      <div>
         <button onClick={() => navigate("/")}>Houses</button>
         <button onClick={() => navigate("/create")}>create</button>
-        <button
-          onClick={logoutHandler}
-          type="button"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="light"
-          className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-        >
-          Log out
-        </button>
+        <button onClick={logoutHandler} type="button">Log out</button>
       </div>
-    </div>
+    </header>
   );
 };
 
